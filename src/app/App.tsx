976f9b2f89a1ec33343ce1322cd4b55e4a89f4ff
Ciahaka <wilddiver.by@gmail.com>
2022-11-29 23:05:58 +0300

    add new component to App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,30 +1,45 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
-import {NavLink, Outlet, Route, Routes} from 'react-router-dom';
+import {NavLink, Outlet, Route, Routes, useNavigate} from 'react-router-dom';
+
+const Profile=()=>{
+  const navigate = useNavigate()
+  return(
+    <div>
+      Profile
+      <Routes>
+        <Route path={'/setting'} element={<div>Setting</div>}/>
+      </Routes>
+      <button onClick={()=>{navigate('/login')}}>Logout</button>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <NavLink to={'/'}>Main</NavLink>----
-      <NavLink to={'/login'}>Login</NavLink>----
+      <NavLink to={'/login'} style={{color: 'green'}}>Login</NavLink>----
       <NavLink to={'/profile'}>Profile</NavLink>----
-      <NavLink to={'/profile/setting'}>Setting</NavLink>---
+      <NavLink to={'/profile/setting'} style={(params) => {
+        return {color: params.isActive ? 'red' : 'black'}
+      }}>Setting</NavLink>---
       <NavLink to={'/menu'}>Menu</NavLink>---
-      <NavLink to={'/menu/submenu'}>Submenu</NavLink>
+      <NavLink to={'/menu/submenu'}>Submenu</NavLink>----
+      <a
+        href={'https://mirpodvoha.by'}
+        target={'_blank'}
+        rel={'no-referrer nofollow no-opener '}
+      >
+        CliK
+      </a>
 
       <Routes>
         <Route path={'/*'} element={<div>404</div>}/>
         <Route path={'/'} element={<div>Main</div>}/>
         <Route path={'/login'} element={<div>Login</div>}/>
         {/*<Route path={'/profile/setting'} element={<div>Setting</div>}/>*/}
-        <Route path={'/profile/*'} element={(
-          <div>
-            Profile
-            <Routes>
-              <Route path={'/setting'} element={<div>Setting</div>}/>
-            </Routes>
-          </div>
-        )}/>
+        <Route path={'/profile/*'} element={<Profile/>}/>
         {/*<Route path={'/menu/submenu'} element={<div>Submenu</div>}/>*/}
         <Route path={'/menu'} element={(
           <div>
